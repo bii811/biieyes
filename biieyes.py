9ad633613f0bd0b7a5eeca8df29e8ea8020221f3
bii811 <phanakhonep@hotmail.com>
2015-03-08 19:30:51 +0700

    Fixed wrong capture
    - Add -S 1

--- biieyes.py
+++ biieyes.py
@@ -7,6 +7,6 @@ if not os.path.exists('biieyespic'):
 	os.makedirs('biieyespic')
 
 while(1):
-	f = os.popen('fswebcam -r 320x240 --greyscale --jpeg 85 -l 3 biieyespic/%d%m%y%H%M%S.jpg')
+	f = os.popen('fswebcam -S 1 -r 320x240 --greyscale --jpeg 85 -l 3 biieyespic/%d%m%y%H%M%S.jpg')
 	out = f.read()
 	print(out)
